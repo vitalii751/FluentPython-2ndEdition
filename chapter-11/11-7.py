24b317ff852a3b0463740f2d4473620f27355608
Vitalii Bakhmach <bahmachronal@gmail.com>
2024-08-29 14:52:43 +0300

    Реалізація магічн методу hash

--- chapter-11/11-7.py
+++ chapter-11/11-7.py
@@ -1,17 +1,22 @@
 class Vector2d:
-    typecode = 'd'
-    
-    def __init__(self, x, y) -> None:
-        self.__x = float(x) # Закритий атрибут
-        self.__y = float(y)
-    
-    @property # помічує метод читання атрибута
-    def x(self): # назва така ж як і атрибута
-        return self.__x # просто повернути
+    typecode = "d"
 
-    @property # так само і для інш атрибута
+    def __init__(self, x, y) -> None:
+        self.__x = float(x)  # Закритий атрибут
+        self.__y = float(y)
+
+    @property  # помічує метод читання атрибута
+    def x(self):  # назва така ж як і атрибута
+        return self.__x  # просто повернути
+
+    @property  # так само і для інш атрибута
     def y(self):
         return self.__y
-    
+
     def __iter__(self):
-        return (i for i in (self.x, self.y)) # всі анші методи, які просто читають значення атрибутів залишаються без змін АЛЕ тепер це self.x виклик метод читання атрибута
+        return (
+            i for i in (self.x, self.y)
+        )  # всі анші методи, які просто читають значення атрибутів залишаються без змін АЛЕ тепер це self.x виклик метод читання атрибута
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
