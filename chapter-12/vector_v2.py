b378d23762646f37d049c0ca9d79956805e412d9
Vitalii Bakhmach <bahmachronal@gmail.com>
2024-08-31 11:28:38 +0300

    Підтрим протокол послідовності просто, якшо можна делегировать роботу атр обьекта, який є послідовністю(self.\_components) АЛЕ тут зріз не дуже працює, бо він повертає обьект класу array

---
+++ chapter-12/vector_v2.py
@@ -0,0 +1,57 @@
+from array import array
+import reprlib
+import math
+
+
+class Vector:
+    typecode = "d"
+
+    def __init__(self, components) -> None:
+        self._components = array(
+            self.typecode, components
+        )  # ств "захищ" атр, який є array
+
+    def __iter__(self):
+        return iter(
+            self._components
+        )  # повертаємо просто ітератор з ітерабильного обьекта
+
+    def __repr__(self) -> str:
+        components = reprlib.repr(
+            self._components
+        )  # отримуємо представлення з лімітною довж. Напр: array('d',[1.0, 2.0, 3.0, ...])
+        components = components[
+            components.find("[") : -1
+        ]  # Видал префікс array('d',[ і також видал остан скобку ')'
+        return f"Vector({components})"
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return bytes([ord(self.typecode)]) + bytes(
+            self._components
+        )  # в байти переводимо масив з числами
+
+    def __eq__(self, other) -> bool:
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.hypot(
+            *self
+        )  # Раніше до Python3.8 юзали: math.sqrt(sum(x * x for x in self))
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(memv)  # передаємо зразу обьект memoryview
+
+    def __len__(self):
+        return len(self._components)
+    
+    def __getitem__(self, index):
+        return self._components[index]
